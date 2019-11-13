d5f8389aec976f005be3059de246f30f4ee9288d
gustavo costa <gustavo.costa@resourceit.com>
2019-11-13 19:36:58 -0300

    parcial

---
+++ spa/src/components/UI/logo/logo.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import estoqueLogo from '../../../assets/Logo-estoque.png'
+
+const logo = () => (
+    <div>
+        <img src={estoqueLogo} />
+    </div>
+)
+
+export default logo
--- spa/src/components/UI/telaInicial/telaInicial.js
+++ spa/src/components/UI/telaInicial/telaInicial.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import classes from './telaInicial.module.css'
+import Logo from '../logo/logo'
 import NavBar from './topNavBar/topNavBar'
 
 const telaInicial = () => (
     <div>
         <NavBar/>
         <h2 className={classes.verde}>Tela inicial...</h2>
+        <Logo/>
     </div>
 )
 
