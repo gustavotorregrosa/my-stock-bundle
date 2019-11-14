8c56d52e04da62fa9e01f0c2834d5ef7690fde8f
gustavo costa <gustavo.costa@resourceit.com>
2019-11-14 19:02:56 -0300

    parcial

--- spa/src/components/UI/logo/logo.js
+++ spa/src/components/UI/logo/logo.js
@@ -1,9 +1,9 @@
 import React from 'react'
-import estoqueLogo from '../../../assets/Logo-estoque.png'
-
+import estoqueLogo from '../../../assets/Logo-estoque.jpg'
+import 'materialize-css/dist/css/materialize.min.css'
 const logo = () => (
     <div>
-        <img src={estoqueLogo} />
+        <img src={estoqueLogo} className="responsive-img" />
     </div>
 )
 
--- spa/src/components/UI/telaInicial/telaInicial.js
+++ spa/src/components/UI/telaInicial/telaInicial.js
@@ -2,13 +2,17 @@ import React from 'react'
 import classes from './telaInicial.module.css'
 import Logo from '../logo/logo'
 import NavBar from './topNavBar/topNavBar'
-import centralizar from '../../../hoc/centralizador/centralizador'
+import estilizar from '../../../hoc/efeitos/aplicaEstilos'
+
 
 
 const telaInicial = () => (
     <div>
         <NavBar />
-       {centralizar(Logo)}
+       {estilizar(Logo, {
+            textAlign: "center",
+            marginTop: "4em"
+       })}
     </div>
 )
 
