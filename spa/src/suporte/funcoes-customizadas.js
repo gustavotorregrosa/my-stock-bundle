4bbd63fbcfdf337b36b8052add29a0560eb19f71
gustavo torregrosa <gustavo.costa@resourceit.com>
2020-01-01 17:21:45 -0300

    parcial

--- spa/src/suporte/funcoes-customizadas.js
+++ spa/src/suporte/funcoes-customizadas.js
@@ -1,25 +1,69 @@
 import store from '../store/store'
 import { verificaLoginLS } from '../store/actions/index'
-
-// export const pegaJWT = () => {
-//     let meuJWT = store.getState()
-//     console.log(meuJWT)
-//     localStorage.setItem("jwt"," teste 123")
-//     store.dispatch({
-//         type: 'VERIFICA_LOGIN_LOCALSTORAGE'
-//     })
-//     setTimeout(() => {
-//         meuJWT = store.getState()
-//         console.log(meuJWT)
-//     }, 2000)
-// }
-
-// export const myFetch = ()
+import { appUrl } from './helper'
 
 const getJwt = () => {
     store.getState().autenticacao.jwt
 }
 
-const atualizaJwtUsuario = () => {
+
+
+const atualizaJwtUsuario =  new Promise((success, reject) => {
     store.dispach(verificaLoginLS())
+    success()
+})
+
+const geraRequest = (rota, obj = null, method = "post") => {
+    if (obj === null) {
+        method = "get"
+    }
+    let h = new Headers()
+    h.set("Content-Type", "application/json")
+    h.set("jwt", getJwt())
+
+    let objRequest = {
+        method,
+        headers: h,
+    }
+
+    if (method != "get") {
+        objRequest.body = JSON.stringify(obj)
+    }
+
+    return new Request(appUrl + rota, objRequest)
 }
+
+
+
+const jwtFetchUnit = requestUnit => {
+    return new Promise((success, reject) => {
+        fetch(requestUnit).then(r => {
+            let status = r.status
+            success(r.json().then(conteudo => {
+                return {
+                    status,
+                    conteudo
+                }
+            }))
+        })
+    })
+}
+
+
+const pFetchGarantido = (url, opcoes) => new Promise((success, reject) => {
+    jwtFetchUnit(geraRequest(url, opcoes)).then(resp => success(resp))
+})
+
+
+export const jwtFetch = (url, opcoes = null) => new Promise((success, reject) => {
+    pFetchGarantido(url, opcoes).then(r => {
+        if(r.status == 203){
+            let objUsuario = r.conteudo
+            localStorage.setItem('jwt', objUsuario.jwt)
+            localStorage.setItem('usuario', JSON.stringify(objUsuario.usuario))
+            atualizaJwtUsuario.then().geraRequest(url, opcoes).then(r => success(r.conteudo))
+
+        }
+        success(r.conteudo)
+    })
+})
